b266974e4bc374ab4752c3635fced36c4282dc0e
Lyna <git@blooym.dev>
2026-04-13 18:47:13 +0100

    chore: use dalamud-packages flake

--- flake.nix
+++ flake.nix
@@ -2,40 +2,37 @@
   description = "Development shell";
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.dalamud = {
+    url = "github:Blooym/dalamud-packages";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
 
   outputs =
-    { nixpkgs, ... }:
+    { nixpkgs, dalamud, ... }:
     let
       forAllSystems =
         function:
         nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (
-          system: function nixpkgs.legacyPackages.${system}
+          system: (function system nixpkgs.legacyPackages.${system})
         );
     in
     {
-      devShells = forAllSystems (pkgs: {
-        default =
-          let
-            # Dalamud is a frequently moving target and will
-            # need to be updated here frequently.
-            #
-            # TODO: Move to dedicate flake and automate.
-            dalamud = pkgs.fetchzip {
-              url = "https://raw.githubusercontent.com/goatcorp/dalamud-distrib/e88388b3eb5fdabd535c7409d6b63537eb82eca0/latest.zip";
-              hash = "sha256-A8aEvAPONUGKqCIWUoUfZFbvHUkw+fAf/Bt5v2m28Qk=";
-              stripRoot = false;
-            };
-          in
-          pkgs.mkShell {
+      devShells = forAllSystems (
+        system: pkgs:
+        let
+          dalamudPkg = dalamud.packages.${system}.release;
+        in
+        {
+          default = pkgs.mkShell {
             packages = with pkgs; [
-              dotnetCorePackages.sdk_10_0
+              dalamudPkg.dotnetSdk
               bashInteractive
             ];
             env = {
-              DOTNET_ROOT = "${pkgs.dotnetCorePackages.sdk_10_0}";
-              DALAMUD_HOME = "${dalamud}";
+              DALAMUD_HOME = dalamudPkg;
             };
           };
-      });
+        }
+      );
     };
 }
